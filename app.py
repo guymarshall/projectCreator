56ef59aab01d446992147f76cca66ed60caa1bf0
Guy Marshall <guymarshall.uk@gmail.com>
2022-06-05 22:14:48 +0100

    Add import for os + os.system call

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 from sys import platform
+import os
 
 username = "testuser"
 
 match platform:
     case "linux" | "linux2":
         default_directory = f"/home/{username}"
+        os.system("ls -lh")
     case "darwin":
         default_directory = f"/Users/{username}"
+        os.system("ls -lh")
     case "win32":
         default_directory = f"/Users/{username}"
+        os.system("dir")
 
 print(f"Directory is {default_directory}")
 
